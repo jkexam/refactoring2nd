2080be3d8664075862bbf037f5846402100aa893
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-01-19 03:30:18 +0900

    * 프로젝트 init

---
+++ refactoring2nd/src/main/java/com/jk/refactoring2nd/Refactoring2ndApplication.java
@@ -0,0 +1,13 @@
+package com.jk.refactoring2nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Refactoring2ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Refactoring2ndApplication.class, args);
+	}
+
+}
---
+++ refactoring2nd/src/test/java/com/jk/refactoring2nd/Refactoring2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.refactoring2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Refactoring2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
