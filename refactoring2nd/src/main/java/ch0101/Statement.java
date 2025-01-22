df6aeceb17b253646d31d3dd9c6f675d87ff1119
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-01-22 22:59:24 +0900

    * p33 명확하게 변수 이름 변경   - ctrl+f6

--- refactoring2nd/src/main/java/ch0101/Statement.java
+++ refactoring2nd/src/main/java/ch0101/Statement.java
@@ -61,26 +61,26 @@ public class Statement {
     }
 
     private static int anoijntFor(Performance perf, Play play) { //값이 바뀌지 않는 변수는 매개변수로 전달
-        int thisAmount = 0; // 변수를 초기화하는 코드
+        int result = 0; // 변수를 초기화하는 코드
 
         switch (play.getType()) {
             case "tragedy": // 비극
-                thisAmount = 40000;
+                result = 40000;
                 if (perf.getAudience() > 30) {
-                    thisAmount += (int) (1000 * (perf.getAudience() - 30));
+                    result += (int) (1000 * (perf.getAudience() - 30));
                 }
                 break;
             case "comedy": // 희극
-                thisAmount = 30000;
+                result = 30000;
                 if (perf.getAudience() > 20) {
-                    thisAmount = (int) (thisAmount + (10000 + 500 * (perf.getAudience() - 20)));
+                    result = (int) (result + (10000 + 500 * (perf.getAudience() - 20)));
                 }
-                thisAmount = (int) (thisAmount + 300 * perf.getAudience());
+                result = (int) (result + 300 * perf.getAudience());
                 break;
             default:
                 throw new IllegalArgumentException(String.format("알 수 없는 장르: %s", play.getType()));
         }
-        return thisAmount; // 함수 안에서 값이 바뀌는 변수 반환
+        return result; // 함수 안에서 값이 바뀌는 변수 반환
     }
 
     @Data
